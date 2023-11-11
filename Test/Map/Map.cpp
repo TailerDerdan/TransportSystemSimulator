a22239db6b2c5b6ef06cb9be8397d6d6db3810bf
TailerDerdan <105823298+TailerDerdan@users.noreply.github.com>
2023-11-11 15:58:26 +0300

    Add Decrypt Chunk

--- Test/Map/Map.cpp
+++ Test/Map/Map.cpp
@@ -12,7 +12,7 @@ void Map::CryptChunk(sf::Vector2i coords, std::string & chunk)
 
 	chunk = coordX + coordY + chunk;
 
-	std::srand(std::time(nullptr));
+	std::srand(uint32_t(std::time(nullptr)));
 	uint32_t key = std::rand();
 
 	uint32_t controlSum = 0;
@@ -29,20 +29,84 @@ void Map::CryptChunk(sf::Vector2i coords, std::string & chunk)
 	std::string keyAsChar = NumToChars(key);
 	chunk = keyAsChar + chunk;
 
-	uint16_t amountOfData = chunk.length();
+	uint16_t amountOfData = uint16_t(chunk.length());
 	for (uint8_t ch : NumToChars(amountOfData))
 	{
 		controlSum += ch;
 	}
 
+	for (uint8_t ch : keyAsChar)
+	{
+		controlSum += ch;
+	}
+
 	controlSum = ~controlSum + 1;
-	chunk = " " + NumToChars(amountOfData) + chunk + NumToChars(controlSum);
-	std::cout << std::format("Key: {}, AmountOfData: {}, ControlSum: {}\n", key, amountOfData, controlSum);
+	chunk = START_CHARACTER + NumToChars(amountOfData) + chunk + NumToChars(controlSum);
 }
 
-void Map::DecryptChunk(std::string& chunk)
+void Map::DecryptChunk(std::ifstream& file)
 {
+	while (!file.eof())
+	{
+		unsigned char ch;
 
+		file.read((char*)&ch, sizeof(uint8_t));
+
+		std::string chunk;
+
+		if (ch != START_CHARACTER)
+		{
+			return;
+		}
+
+		uint32_t sumBytes = 0;
+
+		uint16_t amountBytes = 0;
+		file.read((char*)&amountBytes, sizeof(uint16_t));
+
+		for (uint8_t ch : NumToChars(amountBytes))
+		{
+			sumBytes += ch;
+		}
+
+		uint32_t cryptKey = 0;
+		file.read((char*)&cryptKey, sizeof(uint32_t));
+
+		for (uint8_t ch : NumToChars(cryptKey))
+		{
+			sumBytes += ch;
+		}
+
+		for (uint16_t data = 0; data < amountBytes; data++)
+		{
+			file.read((char*)&ch, sizeof(char));
+			ch = ch ^ cryptKey;
+			chunk.push_back(ch);
+			if (data < amountBytes - sizeof(sumBytes))
+			{
+				sumBytes += uint8_t(ch);
+			}
+		}
+
+		uint32_t controlSum = 0;
+		std::string controlSumStr = chunk.substr(chunk.length() - 4, 4);
+
+		std::transform(controlSumStr.begin(), controlSumStr.end(), controlSumStr.begin(), [cryptKey](unsigned char ch) { return ch ^ cryptKey; });
+
+		controlSum = CharsToNum(controlSumStr);
+		WrapVector2i coords;
+		coords.v.x = CharsToNum(chunk.substr(0, 4));
+		coords.v.y = CharsToNum(chunk.substr(4, 4));
+
+		if (controlSum != (~sumBytes + 1))
+		{
+			return;
+		}
+		chunk.erase(chunk.length() - 4, 4);
+		chunk.erase(0, 8);
+
+		allChunks.emplace(std::pair(coords, chunk));
+	}
 }
 
 void Map::RenderMap(bool LoadUpload)
@@ -58,64 +122,11 @@ void Map::RenderMap(bool LoadUpload)
 			std::cout << "Can't open data file!\n";
 			return;
 		}
-
-		while (!file.eof())
-		{
-			unsigned char ch;
-
-			file.read((char*)&ch, sizeof(uint8_t));
-			
-			std::string chunk;
-
-			if (ch == ' ')
-			{
-				uint32_t sumBytes = 0;
-
-				uint16_t amountBytes = 0; 
-				file.read((char*)&amountBytes, sizeof(uint16_t));
-
-				uint32_t cryptKey = 0;
-				file.read((char*)&cryptKey, sizeof(uint32_t));
-				
-				for (uint16_t data = 0; data < amountBytes; data++)
-				{
-					file.read((char*)&ch, sizeof(char));
-					ch = ch ^ cryptKey;
-					chunk.push_back(ch);
-					sumBytes += uint8_t(ch);
-				}
-
-				std::string x = chunk.substr(0, 4);
-				std::string y = chunk.substr(4, 4);
-				
-
-				uint32_t controlSum = 0;
-				std::string cs = chunk.substr(chunk.length() - 4, 4);
-
-				for (size_t i = 0; i < cs.length(); i++)
-				{
-					controlSum += cs[cs.length() - i];
-					controlSum = controlSum << 8;
-				}
-
-				if (controlSum != ~(sumBytes)+1)
-				{
-					return;
-				}
-			}
-			else
-			{
-				return;
-			}
-			//allChunks.emplace(std::pair());
-		}
+		DecryptChunk(file);
 		stateLoadUpload = false;
 	}
 	else
 	{
-		allChunks.emplace(sf::Vector2i{ 1651, 193212 }, "abaabbabababababababaaabab");
-		allChunks.emplace(sf::Vector2i{ -1231, -666 }, "sa121212sajjsajasjasasjhass");
-
 		std::ofstream file;
 		file.open("DATA.txt", std::ios::trunc);
 
--- Test/Map/Map.h
+++ Test/Map/Map.h
@@ -36,11 +36,11 @@ private:
 	std::unordered_map<WrapVector2i, std::string, WrapVector2iHasher> allChunks;
 	bool stateLoadUpload = true;
 	const uint8_t CHUNK_DELIMETR = 32;
+	const char START_CHARACTER = ' ';
 
 	void CryptChunk(sf::Vector2i coords, std::string& chunk);
-	void DecryptChunk(std::string& chunk);
+	void DecryptChunk(std::ifstream& file);
 	
-
 	template <std::integral T>
 	std::string NumToChars(T v)
 	{
@@ -52,5 +52,19 @@ private:
 			v = v >> 8;
 		}
 		return l;
+	}
+
+	uint32_t CharsToNum(std::string str)
+	{
+		uint32_t num = 0;
+		for (size_t i = 0; i < str.length(); i++)
+		{
+			num += uint8_t(str[str.length() - i - 1]);
+			if (i < 3)
+			{
+				num = num << 8;
+			}
+		}
+		return num;
 	}
 };
